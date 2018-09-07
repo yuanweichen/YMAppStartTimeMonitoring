82b047658d6a9dc1d1db2e9f79ee98ec71fb6407
yuanwei <yuanwei@rrkd.cn>
2018-09-07 14:01:59 +0800

    action

--- YMAppStartTimeMonitoring/YMAppStartTimeMonitoring/AppDelegate.m
+++ YMAppStartTimeMonitoring/YMAppStartTimeMonitoring/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "YMStartTimeMonitoring.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [[YMStartTimeMonitoring sharedMonitoring] start];
+    [[YMStartTimeMonitoring sharedMonitoring] showInfoWithDescribe:@"初始化"];
+    [[YMStartTimeMonitoring sharedMonitoring] stop];
     return YES;
 }
 
